2017414fd40b30cc149a55f9b91d7fe6e618a4cb
Somnath <somnath.ganguly.cse@gmail.com>
2024-07-16 15:13:44 +0530

    treasury mapping config added

--- Helpers/MappingConfig.cs
+++ Helpers/MappingConfig.cs
@@ -14,7 +14,7 @@ namespace master.Helpers
             CreateMap<masterSCHEME_HEADModel, SchemeHead>().ReverseMap();
             CreateMap<masterDepartmentModel, Department>().ReverseMap();
             CreateMap<MasterManegmentModel, MajorHead>().ReverseMap();
-            
+            CreateMap<masterTreasuryModel, Treasury>().ReverseMap();
 
         }
     }
