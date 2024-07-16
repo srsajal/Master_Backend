9a156b153f613e8264b6e06308eccdc7b56dd1b3
Somnath <somnath.ganguly.cse@gmail.com>
2024-07-16 14:31:30 +0530

    mapping added

--- Helpers/MappingConfig.cs
+++ Helpers/MappingConfig.cs
@@ -12,6 +12,7 @@ namespace master.Helpers
             CreateMap<masterDDOModel, Ddo>().ReverseMap();
             CreateMap<masterSCHEME_HEADModel, SchemeHead>().ReverseMap();
             CreateMap<masterDepartmentModel, Department>().ReverseMap();
+            CreateMap<masterTreasuryModel, Treasury>().ReverseMap();
 
         }
     }
