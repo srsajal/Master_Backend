850a9055778220b8161a974265751cdd4edf55ab
Rahul Roy <roy172693@gmail.com>
2024-07-19 14:25:18 +0530

    schemehead update

---
+++ Dto/SchemeMinorheadfromMINORHEADIdDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace master.Dto
+{
+    public class SchemeMinorheadfromMINORHEADIdDTO
+    { public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
